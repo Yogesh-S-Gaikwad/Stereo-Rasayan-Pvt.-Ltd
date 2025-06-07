60e80d8cc5e0f9ea1bf684b89b8551710d956877
Yogesh Gaikwad <yogeshgaikwad0330@gmai.com>
2025-06-07 17:18:00 +0530

    footer

--- script.js
+++ script.js
@@ -4,3 +4,12 @@
   toggleBtn.addEventListener('click ', () => {
     mobileNav.classList.toggle('active');
   });
+
+  function subscribe() {
+      const email = document.getElementById('emailInput').value;
+      if (email) {
+        alert("Thank you for subscribing: " + email);
+      } else {
+        alert("Please enter a valid email.");
+      }
+    }
